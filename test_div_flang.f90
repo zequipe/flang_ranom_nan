f59a082d8cbdb51415586a97302b0ba2c724b332
zaikunzhang <zaikunzhang@gmail.com>
2026-02-11 15:55:58 +0100

    correct comments

--- test_div_flang.f90
+++ test_div_flang.f90
@@ -1,5 +1,5 @@
-! test_div.f90
-program test_div
+! test_div_flang.f90
+program test_div_flang
 use iso_fortran_env, only : RP => REAL32
 
 implicit none
@@ -33,4 +33,4 @@ print * , '------------------'
 print *, a, '|', a/c, '|', b
 print *, a, '|', (a/c)**2, '|', b**2
 
-end program test_div
+end program test_div_flang
